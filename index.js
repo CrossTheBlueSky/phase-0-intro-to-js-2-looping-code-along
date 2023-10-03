2702735810863c704205f9bfb05bd83fa1a3acea
Derek Mercedes <mercedes.derek@gmail.com>
2023-10-02 20:36:47 -0600

    loops with while and for

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+    const thankYouArray = []
+    for(let i = 0; i < names.length; i++){
+        
+        thankYouArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouArray
+}
+
+function countDown(num){
+    while(num >= 0){
+        console.log(num)
+        num--
+    }
+}
